ce329b97cd64b1c9a5bf068e3ce13e6a70dfcc65
Eric Wallace <90478940+wallacee1@users.noreply.github.com>
2026-03-11 18:44:59 -0400

    Removed duplicated files

--- tfapp/tfapp/pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-from attendance import views as attendance_views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
--- tfapp/tfapp/timeclock/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "timeclock"
-
-urlpatterns = [
-    path('', views.timeclock_home, name='timeclock_home'),
-    path("edit/<int:pk>/", views.edit_entry, name="edit_entry"),
-]
